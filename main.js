c4c6aa094729f5572d418fb4e945774220b94d8c
leandro <leandros8w@gmail.com>
2024-01-25 19:08:17 -0300

    up

---
+++ main.js
@@ -0,0 +1,7 @@
+const display1EL = document.querySelector('.display-1');
+const display2EL = document.querySelector('.display-1');
+const numbersEL = document.querySelectorAll('.number');
+const operationEL = document.querySelectorAll('.operation');
+const equalES = document.querySelector('.equal');
+const clearAllEL = document.querySelectorAll('.all_clear');
+const clearLastEl = document.querySelectorAll('.last_entity_clear');
